ba65e78201332b53fbd1f98abe8ee0a5208c0200
Javier <javiermacasaet@gmail.com>
2026-03-02 00:02:36 +0800

    Implement localevents app: models, views, templates, admin integration

--- localevents/views.py
+++ localevents/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Event
 
-# Create your views here.
+def event_list(request):
+    events = Event.objects.all()
+    context = {
+        'events': events,
+    }
+    return render(request, 'event_list.html', context)
+
+def event_detail(request, event_id):
+    try:
+        event = Event.objects.get(id=event_id)
+    except Event.DoesNotExist:
+        raise Http404("Event does not exist")
+
+    context = {
+        'event': event,
+    }
+    return render(request, 'event_detail.html', context)
