66e1cc29d26faba24d90ed3f57967ef0001c8d94
PolarBear <phoebe.h.chua@student.ateneo.edu>
2026-03-27 16:13:45 +0800

    PEP 8

--- merchstore/models.py
+++ merchstore/models.py
@@ -17,25 +17,46 @@ class ProductType(models.Model):
         verbose_name_plural = 'product types'
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    product_type = models.ForeignKey(ProductType, on_delete = models.SET_NULL, null=True, blank=True)
-    owner = models.ForeignKey(Profile, on_delete = models.CASCADE)
-    product_image = models.ImageField(upload_to='images/')
+    name = models.CharField(
+        max_length=255
+    )
+    product_type = models.ForeignKey(
+        ProductType, 
+        on_delete = models.SET_NULL, 
+        null=True,
+        blank=True
+    )
+    owner = models.ForeignKey(
+        Profile, on_delete = models.CASCADE
+    )
+    product_image = models.ImageField(
+        upload_to='images/'
+    )
     description = models.TextField()
-    price = models.DecimalField(max_digits= 10, decimal_places=2)
+    price = models.DecimalField(
+        max_digits= 10,
+        decimal_places=2
+    )
     stock = models.PositiveIntegerField()
     STATUS = {
         "A": "available",
         "OS": "on sale",
         "OOS": "out of stock"
     }
-    status = models.CharField(max_length = 3, choices = STATUS, default = "A")
+    status = models.CharField(
+        max_length = 3,
+        choices = STATUS,
+        default = "A"
+    )
 
     def __str__(self):
         return self.name
         
     def get_absolute_url(self):
-        return reverse('merchstore:merchstore-detail', args=[str(self.id)])
+        return reverse(
+            'merchstore:merchstore-detail',
+            args=[str(self.id)]
+        )
     
     class Meta:
         ordering = ['name']
@@ -43,9 +64,19 @@ class Product(models.Model):
         verbose_name_plural = 'products'
 
 class Transaction(models.Model):
-    buyer = models.ForeignKey(Profile, on_delete = models.SET_NULL, null=True, blank=True)
-    product = models.ForeignKey(Product, on_delete = models.CASCADE)
-    amount = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    buyer = models.ForeignKey(
+        Profile,
+        on_delete = models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete = models.CASCADE
+    )
+    amount = models.PositiveIntegerField(
+        validators=[MinValueValidator(1)]
+    )
     STATUS = {
         "OC": "on cart",
         "TP": "to pay",
@@ -53,5 +84,10 @@ class Transaction(models.Model):
         "TR": "to receive",
         "D": "delivered"
     }
-    status = models.CharField(max_length = 5, choices = STATUS)
-    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length = 5,
+        choices = STATUS
+    )
+    created_on = models.DateTimeField(
+        auto_now_add=True
+    )
--- merchstore/urls.py
+++ merchstore/urls.py
@@ -7,12 +7,36 @@ from .views import (
 
 urlpatterns = [
     path('', index, name='index'),
-    path('items/', ProductListView.as_view(), name="merchstore-list"),
-    path('item/<int:pk>/', ProductDetailView.as_view(), name="merchstore-detail"),
-    path('item/add/', ProductCreateView.as_view(), name="merchstore-create"),
-    path('item/<int:pk>/edit', ProductUpdateView.as_view(), name="merchstore-update"),
-    path('cart/', CartView.as_view(), name="merchstore-cart"),
-    path('transactions/', TransactionsListView.as_view(), name="merchstore-transactions"),
+    path(
+        'items/',
+        ProductListView.as_view(),
+        name="merchstore-list"
+    ),
+    path(
+        'item/<int:pk>/',
+        ProductDetailView.as_view(),
+        name="merchstore-detail"
+    ),
+    path(
+        'item/add/',
+        ProductCreateView.as_view(),
+        name="merchstore-create"
+    ),
+    path(
+        'item/<int:pk>/edit',
+        ProductUpdateView.as_view(),
+        name="merchstore-update"
+    ),
+    path(
+        'cart/',
+        CartView.as_view(),
+        name="merchstore-cart"
+    ),
+    path(
+        'transactions/',
+        TransactionsListView.as_view(),
+        name="merchstore-transactions"
+    ),
 ]
 
 app_name = "merchstore"
